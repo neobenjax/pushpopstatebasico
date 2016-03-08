a8da65bce750c97059e59547ac324f369a47412e
neobenjax <besaze@hotmail.com>
2016-03-07 18:31:35 -0600

    Archivos de ejemplo para pushypopstate

---
+++ index_files/app.js
@@ -0,0 +1,69 @@
+(function(){
+
+  "use strict";
+
+  var container = document.querySelector('.gallery'),
+    imgs = document.querySelectorAll('img'),
+    textWrapper = document.querySelector('.highlight'),
+    content = document.querySelector('.content'),
+    defaultTitle = "Select your Ghostbuster!";
+
+  function updateText(content){
+    textWrapper.innerHTML = content;
+  }
+
+  function requestContent(file){
+    $('.content').load(file + ' .content');
+  }
+
+  function removeCurrentClass(){
+    for(var i = 0; i < imgs.length; i++){
+      imgs[i].classList.remove('current');
+    }
+  }
+
+  function addCurrentClass(elem){
+    removeCurrentClass();
+    var element = document.querySelector("." + elem);
+    element.classList.add('current');
+  }
+
+  container.addEventListener('click', function(e){
+    console.log("Click");
+    console.log(e);
+    if(e.target != e.currentTarget){
+      e.preventDefault();
+      var data = e.target.getAttribute('data-name'),
+        url = data + ".php";
+      addCurrentClass(data);
+      console.log('Data:'+data);
+      console.log('URL:'+url);
+      history.pushState(data, null, url);
+      updateText(data);
+      requestContent(url);
+      document.title = "Ghostbuster | " + data;
+    }
+    e.stopPropagation();
+  }, false);
+
+
+  window.addEventListener('popstate', function(e){
+    console.log("Post");
+    console.log("State:",e);
+    var character = e.state;
+
+    console.log("character:",character);
+
+    if (character == null) {
+      removeCurrentClass();
+      textWrapper.innerHTML = " ";
+      content.innerHTML = " ";
+      document.title = defaultTitle;
+    } else {
+      updateText(character);
+      requestContent(character + ".php");
+      addCurrentClass(character);
+      document.title = "Ghostbuster | " + character;
+    }
+  })
+})();
